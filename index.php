db0938b5f6d7d4a181d37d411c28b0e1275fc6a5
Renee Holter <holter.renee@student.greenriver.edu>
2022-06-05 16:04:46 -0400

    adding in the home.html

--- index.php
+++ index.php
@@ -1,1 +1,59 @@
 <?php
+
+// Turn on error reporting
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
+
+// Require the necessary files
+require_once('vendor/autoload.php');
+
+// Start the session
+session_start();
+
+// Test DataLayer class
+//$dataLayer = new DataLayer();
+
+// Create instance of the base class
+$f3 = Base::instance();
+
+// Creating an instance of the controller class
+//$con = new Controller($f3);
+
+// Define a default route
+// Home page rendering
+$f3->route('GET /', function()
+{
+    // Displaying the page
+    global $con;
+    $con->home();
+});
+
+$f3->route('GET|POST /personalInfo', function()
+{
+    // Displaying the page
+    global $con;
+    $con->personalInfo();
+});
+
+$f3->route('GET|POST /profile', function()
+{
+    // Displaying the page
+    global $con;
+    $con->profile();
+});
+
+$f3->route('GET|POST /interests', function($f3)
+{
+    // Displaying the page
+    global $con;
+    $con->interests();
+});
+
+$f3->route('GET|POST /profileSummary', function()
+{
+    global $con;
+    $con->profileSummary();
+});
+
+// Run fat free
+$f3->run();
